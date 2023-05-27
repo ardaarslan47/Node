91ed694490453cf5529ba4a5fd538d5690ec0e0f
ardaarslan47 <a.ardaarslan@hotmail.com>
2023-05-27 21:33:49 +0300

    templating updated

--- templating/app.js
+++ templating/app.js
@@ -1,7 +1,7 @@
-import { match } from "assert";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import redditData from "./data.json" assert { type: "json" };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,18 +9,33 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/random', (req, res) => {
-    const randomNum = Math.floor (Math.random() * 10) + 1
-    res.render('random', { randomNum: randomNum })
-})
+app.get("/cats", (req, res) => {
+  const cats = ["caki", "soso", "susi"];
+  res.render("cats", { cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const subs = redditData[subreddit];
+  if (subs) {
+    res.render("subreddit", { ...subs });
+  } else {
+    res.render("subNotFound", { subreddit });
+  }
+});
+
+app.get("/random", (req, res) => {
+  const randomNum = Math.floor(Math.random() * 10) + 1;
+  res.render("random", { randomNum: randomNum });
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-})
+  console.log(`listening on port ${port}`);
+});
